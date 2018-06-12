1290ac0fa16d44f7a1ca2a9850fcae587a7c7faf
Matthias Jenny <matthiascjenny@gmail.com>
2018-06-12 10:58:25 -0400

    Add extensions list

---
+++ extensions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+code --install-extension CraigMaslowski.erb
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension ZneuRay.erb-vscode-snippets
+code --install-extension akamud.vscode-theme-onedark
+code --install-extension castwide.solargraph
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension doublefint.pgsql
+code --install-extension ecmel.vscode-html-css
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-mssql.mssql
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension rebornix.Ruby
+code --install-extension vincaslt.highlight-matching-tag
+code --install-extension vortizhe.simple-ruby-erb
