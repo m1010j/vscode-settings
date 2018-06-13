9013e42f2395431eccf2c251f997bc0326c00d27
Matthias Jenny <matthiascjenny@gmail.com>
2018-06-12 20:24:45 -0400

    Update extensions from home machine

--- extensions.sh
+++ extensions.sh
@@ -2,18 +2,25 @@
 
 code --install-extension CraigMaslowski.erb
 code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension TimonVS.ReactSnippetsStandard
 code --install-extension ZneuRay.erb-vscode-snippets
+code --install-extension abusaidm.html-snippets
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension castwide.solargraph
+code --install-extension codepunkt.vscode-javascript-snippets
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension doublefint.pgsql
+code --install-extension dzannotti.vscode-babel-coloring
 code --install-extension ecmel.vscode-html-css
 code --install-extension esbenp.prettier-vscode
+code --install-extension mohsen1.prettify-json
 code --install-extension ms-mssql.mssql
 code --install-extension ms-vscode.atom-keybindings
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension rebornix.Ruby
+code --install-extension sdras.night-owl
 code --install-extension vincaslt.highlight-matching-tag
 code --install-extension vortizhe.simple-ruby-erb
+code --install-extension wayou.vscode-todo-highlight
 
 # to update, run: code --list-extensions | xargs -L 1 echo code --install-extension
