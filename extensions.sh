c3845bdcdc4b00a0246e0123b3b1d980386c2afc
Matthias Jenny <matthiascjenny@gmail.com>
2019-02-28 18:07:26 -0500

    Update extensions

--- extensions.sh
+++ extensions.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-code --install-extension CraigMaslowski.erb
 code --install-extension TimonVS.ReactSnippetsStandard
 code --install-extension ZneuRay.erb-vscode-snippets
 code --install-extension abusaidm.html-snippets
+code --install-extension akamud.vscode-theme-onedark
 code --install-extension bibhasdn.django-html
 code --install-extension castwide.solargraph
 code --install-extension codepunkt.vscode-javascript-snippets
+code --install-extension CraigMaslowski.erb
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension doublefint.pgsql
 code --install-extension dzannotti.vscode-babel-coloring
 code --install-extension ecmel.vscode-html-css
 code --install-extension esbenp.prettier-vscode
+code --install-extension flowtype.flow-for-vscode
+code --install-extension kisstkondoros.csstriggers
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension lsadam0.ReactFlowSnippets
 code --install-extension mkaufman.HTMLHint
 code --install-extension mohsen1.prettify-json
 code --install-extension ms-mssql.mssql
@@ -25,12 +30,15 @@ code --install-extension PKief.material-icon-theme
 code --install-extension rebornix.ruby
 code --install-extension RoscoP.ActiveFileInStatusBar
 code --install-extension sdras.night-owl
+code --install-extension shinnn.stylelint
 code --install-extension sleistner.vscode-fileutils
+code --install-extension TimonVS.ReactSnippetsStandard
 code --install-extension toasty-technologies.octave
 code --install-extension vincaslt.highlight-matching-tag
 code --install-extension vortizhe.simple-ruby-erb
 code --install-extension wayou.vscode-todo-highlight
 code --install-extension wix.vscode-import-cost
+code --install-extension ZneuRay.erb-vscode-snippets
 
 
 # to update, run: code --list-extensions | xargs -L 1 echo code --install-extension
